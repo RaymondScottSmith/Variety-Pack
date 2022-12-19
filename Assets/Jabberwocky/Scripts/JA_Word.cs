6861032d00e4c51ce02a92b4dbc35b6ebb4973fa
RaymondScottSmith <rayscottsmith1@gmail.com>
2022-12-19 11:09:51 -0600

    Adding JA_Word class
    So named because I might need the Word name again.

---
+++ Assets/Jabberwocky/Scripts/JA_Word.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class JA_Word
+{
+    public string word;
+
+    public JA_Word(string myWord)
+    {
+        word = myWord;
+    }
+}
--- Assets/Jabberwocky/Scripts/WordManager.cs
+++ Assets/Jabberwocky/Scripts/WordManager.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class WordManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<JA_Word> wordList;
+    
+    
 }
